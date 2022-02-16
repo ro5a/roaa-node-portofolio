5e093e4cfc25657e9fed0ffc732a5c886c701ee4
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-16 15:54:50 +0300

    craete home page with nodejs ejs method

---
+++ Portfolio-node-ejs/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+app.listen(8080);
+console.log('Server is listening on port 8080');
